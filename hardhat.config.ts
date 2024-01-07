d78a7740ecd6cb0353d136d0f6c5968783db4053
Lila Rest <mail@lila.rest>
2024-01-07 15:03:16 +0100

    chore: prepare Hardhat and Foundry envs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,73 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { type HardhatUserConfig } from "hardhat/config";
+import "hardhat-contract-sizer";
+import "hardhat-deploy";
+import "@nomicfoundation/hardhat-verify";
+
+// Retrieve deployer private key from secrets.json (if available)
+let deployerPrivateKey: string | undefined;
+try {
+  const secrets = require("./secrets.json");
+  deployerPrivateKey = secrets.CONTRACT_DEPLOYER_PRIVATE_KEY;
+} catch (e) {}
+
+// Retrieve Lineascan API key from secrets.json (if available)
+let etherscanApiKey: string | undefined;
+try {
+  const secrets = require("./secrets.json");
+  etherscanApiKey = secrets.ETHERSCAN_API_KEY;
+} catch (e) {}
 
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.20",
     settings: {
+      evmVersion: "london",
       optimizer: {
         enabled: true,
         runs: 200,
       },
-    }
+    },
+  },
+  paths: {
+    sources: "./src",
+    cache: "./hardhat/cache",
+    artifacts: "./hardhat/artifacts",
+    deploy: "./hardhat/deploy",
+    deployments: "./hardhat/deployments",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
   },
   networks: {
-    hardhat: {},
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337,
+    hardhat: {
+      // Is used to fix gas estimation error
+      // See: https://github.com/NomicFoundation/hardhat/issues/3089#issuecomment-1366428941
+      initialBaseFeePerGas: 0,
+    },
+    ethereum: {
+      chainId: 1,
+      url: "https://mainnet.infura.io/v3/05368c74554249babb6f126ccf325401",
+      accounts: deployerPrivateKey ? [deployerPrivateKey] : [],
+      saveDeployments: true,
+      verify: {
+        etherscan: {
+          apiKey: etherscanApiKey,
+          apiUrl: "https://api.lineascan.build",
+        },
+      },
+    },
+    ethereumGoerli: {
+      chainId: 5,
+      url: "https://goerli.infura.io/v3/05368c74554249babb6f126ccf325401",
+      accounts: deployerPrivateKey ? [deployerPrivateKey] : [],
+      saveDeployments: true,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      ethereum: etherscanApiKey!,
     },
   },
   defaultNetwork: "hardhat",
